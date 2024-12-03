3087b97752b3253ea26a3b01fecf0b6f238e9245
nan3r0 <sabingrg11@yahoo.com>
2024-12-03 18:18:34 +0000

    Add files via upload

---
+++ npcmode/npcmode.js
@@ -0,0 +1,22 @@
+
+var input = document.getElementById("input");
+function clr(){
+   input.value = '';
+}
+function typing(x){
+   input.value += x;
+}
+
+const timerElement = document.getElementById('timer');
+
+let timeLeft = 20; // Adjust the time in seconds as needed
+
+const timerInterval = setInterval(() => {
+  timerElement.textContent = `Time remaining: ${timeLeft} seconds`;
+  timeLeft--;
+
+  if (timeLeft < 0) {
+    clearInterval(timerInterval);
+    window.location.href = '/modes/modeselection.html'; // Replace with the actual URL
+  }
+}, 1000);
